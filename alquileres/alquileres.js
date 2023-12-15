6883f76e7265ae08de673939e21b991c23b262be
FabianCiceia <ciceiafabian@gmail.com>
2023-12-15 20:26:52 -0300

    estilado de paguina alquileres y orientacion de colores a una paleta monocrometica

--- alquileres/alquileres.js
+++ alquileres/alquileres.js
@@ -3,33 +3,29 @@ var alquileresHTML = '';
 datosAlquileres.forEach(function(alquiler) {
 
 
-    alquileresHTML = `
-        <div class="alquiler" id="arquiler.${alquiler.id}"> 
-            
-            <h2>${alquiler.nombre}</h2>
-            <p><strong>Descripción:</strong> ${alquiler.descripcion}</p>
-            <p><strong>Número de piezas:</strong> ${alquiler.nroPiezas}</p>
-            <p><strong>Precio mensual:</strong> ${alquiler.precioMensual}</p>
-            <p><strong>Depósito de garantía:</strong> ${alquiler.depositoGarantia}</p>
-            <p><strong>Número de contacto:</strong> ${alquiler.nroContacto}</p>
-          
-            
+    alquileresHTML += `
 
+        <a href="#">
+        <div class="imagen">
+            <img src="${alquiler.imagen}" alt="Imagen 1">
+            <div class="capa">
+                <h3  class="datosArq">
+                    <p>${alquiler.descripcion}</p>
+                    <p><strong>Número de piezas:</strong> ${alquiler.nroPiezas}</p>
+                </h3>
+                <p class="datosArq">
+                    <p><strong>Precio mensual:</strong> ${alquiler.precioMensual}</p>
+                    <p><strong>Depósito de garantía:</strong> ${alquiler.depositoGarantia}</p>
+                    <p><strong>Número de contacto:</strong> ${alquiler.nroContacto}</p>
+                </p>
+            </div>
         </div>
+    </a>
         
     `;
 
 });
 
-document.getElementById('alquileres').innerHTML = alquileresHTML;
-
-var alquilerElement = document.querySelectorAll('.alquiler');
-
-for(var i=0; i< alquilerElement.length; i++){
-    var img = datosAlquileres[i].imagen;
-    alquilerElement[i].style.backgroundImage =  `url(${img})`;  
-
-
-}
+document.getElementById('alquiler').innerHTML = alquileresHTML;
 
 
