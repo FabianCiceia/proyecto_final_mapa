7caa5b8c2cc62b91f509eec09810c64bffb8f2c3
FacundoMM <facundoezem@gmail.com>
2023-12-16 14:31:04 -0300

    actualizacion de errores

--- mapa/mapa_marcador.js
+++ mapa/mapa_marcador.js
@@ -41,6 +41,8 @@ map.on('zoomend', function () {
  
   }
 
+
+
   // Itera sobre los marcadores y actualiza el tamaño del icono
   Object.values(alquileres).forEach(function (marker) {
     marker.setIcon(L.icon({
@@ -52,7 +54,116 @@ map.on('zoomend', function () {
   });
 });
 
-
+var datosAlquileres = 
+[
+    {
+        "id":"0",
+        "nombre": "Edificio Newton",
+        "imagen":"https://cdn.pixabay.com/photo/2015/10/20/18/57/furniture-998265_1280.jpg",
+        "descripcion": "Un apartamento con politica sin mascotas y con estacionamiento.",
+        "nroPiezas": "1",
+        "precioMensual": "500000",
+        "depositoGarantia": "250000",
+        "nroContacto": "0981625487",
+        "latitud": "-27.30862857824979",
+        "longitud": "-55.88903903961182"
+    },
+    {
+        "id":"1",
+        "nombre": "El azul",
+        "imagen":"https://cdn.pixabay.com/photo/2016/11/30/08/48/bedroom-1872196_1280.jpg   ",
+        "descripcion": "con politica, luz y agua incluido en el alquiler",
+        "nroPiezas": "1",
+        "precioMensual": "800000",
+        "depositoGarantia": "1000000",
+        "nroContacto": "0981541234",
+        "latitud": "-27.307956493549586",
+        "longitud": "-55.8876657485962"
+    },
+    {
+        "id":"2",
+        "nombre": "Edificio San Miguel II",
+        "imagen":"https://cdn.pixabay.com/photo/2016/09/19/17/20/home-1680800_1280.jpg",
+        "descripcion": "Un apartamento con politica sin mascotas",
+        "nroPiezas": "1",
+        "precioMensual": "850000",
+        "depositoGarantia": "500000",
+        "nroContacto": "0981658985",
+        "latitud": "-27.308275853453686",
+        "longitud": "-55.88714003562928"
+    },
+    {
+        "id":"3",
+        "nombre": "Edificio L",
+        "imagen":"https://cdn.pixabay.com/photo/2017/07/09/03/19/home-2486092_1280.jpg",
+        "descripcion": "Un apartamento con politica sin mascotas",
+        "nroPiezas": "2",
+        "precioMensual": "850000",
+        "depositoGarantia": "500000",
+        "nroContacto": "0981658658",
+        "latitud": "-27.303938200107797",
+        "longitud": "-55.88535904884339"
+    },
+    {
+        "id":"4",
+        "nombre": "Residencial Arminio",
+        "imagen":"https://cdn.pixabay.com/photo/2017/03/28/12/13/chairs-2181968_1280.jpg",
+        "descripcion": "Un apartamento con politica sin mascotas",
+        "nroPiezas": "4",
+        "precioMensual": "2000000",
+        "depositoGarantia": "1500000",
+        "nroContacto": "098165862",
+        "latitud": "-27.305272880732932",
+        "longitud": "-55.89010655879975"
+    },
+    {
+        "id":"5",
+        "nombre": "Edificio Green",
+        "imagen":"https://cdn.pixabay.com/photo/2016/11/30/08/46/living-room-1872192_1280.jpg",
+        "descripcion": "Departamento ecológico con paneles solares",
+        "nroPiezas": "2",
+        "precioMensual": "900000",
+        "depositoGarantia": "800000",
+        "nroContacto": "0987123456",
+        "latitud": "-27.307983",
+        "longitud": "-55.888422"
+    },
+    {
+        "id":"6",
+        "nombre": "Residencial Oasis",
+        "imagen":"https://cdn.pixabay.com/photo/2016/11/30/08/48/bedroom-1872196_1280.jpg",
+        "descripcion": "Confortable departamento con áreas verdes",
+        "nroPiezas": "2",
+        "precioMensual": "950000",
+        "depositoGarantia": "850000",
+        "nroContacto": "0987987654",
+        "latitud": "-27.307937427257592",
+        "longitud": "-55.88520348072053"
+    },{
+        "id":"7",
+        "nombre": "El Palmar",
+        "imagen":"https://cdn.pixabay.com/photo/2015/10/20/18/57/furniture-998265_1280.jpg",
+        "descripcion": "Zona tranquila y segura",
+        "nroPiezas": "2",
+        "precioMensual": "850000",
+        "depositoGarantia": "750000",
+        "nroContacto": "0987345678",
+        "latitud": "-27.31131210991641",
+        "longitud": "-55.884790420532234"
+    },
+    {
+        "id":"8",
+        "nombre": "Residencial Plaza",
+        "imagen":"https://cdn.pixabay.com/photo/2017/03/28/12/11/chairs-2181960_1280.jpg",
+        "descripcion": "Apartamento moderno con amenities",
+        "nroPiezas": "1",
+        "precioMensual": "750000",
+        "depositoGarantia": "700000",
+        "nroContacto": "0987987654",
+        "latitud": "-27.308190055360797",
+        "longitud": "-55.88356733322144"
+    }
+];
 for(let i = 0; i < datosAlquileres.length;i++ ){
   
   var alquilerActual = datosAlquileres[i];
