66f32ffa94a772dacb7140e2d3e77be27f57da42
FabianCiceia <ciceiafabian@gmail.com>
2023-12-16 12:00:32 -0300

    Eliminando archivos inecesarios

--- alquileres/alquileres.js
+++ alquileres/alquileres.js
@@ -1,31 +1,27 @@
 var alquileresHTML = '';
-
 datosAlquileres.forEach(function(alquiler) {
 
 
     alquileresHTML += `
 
         <a href="#">
-        <div class="imagen">
-            <img src="${alquiler.imagen}" alt="Imagen 1">
-            <div class="capa">
-                <h3  class="datosArq">
-                    <p>${alquiler.descripcion}</p>
-                    <p><strong>Número de piezas:</strong> ${alquiler.nroPiezas}</p>
-                </h3>
-                <p class="datosArq">
-                    <p><strong>Precio mensual:</strong> ${alquiler.precioMensual}</p>
-                    <p><strong>Depósito de garantía:</strong> ${alquiler.depositoGarantia}</p>
-                    <p><strong>Número de contacto:</strong> ${alquiler.nroContacto}</p>
-                </p>
+            <div class="imagen">
+                <img src="${alquiler.imagen}" alt="Imagen 1">
+                <div class="capa">
+                    <h3  class="datosArq">
+                        <p>${alquiler.descripcion}</p>
+                        <p><strong>Número de piezas:</strong> ${alquiler.nroPiezas}</p>
+                    </h3>
+                    <p class="datosArq">
+                        <p><strong>Precio mensual:</strong> ${alquiler.precioMensual}</p>
+                        <p><strong>Depósito de garantía:</strong> ${alquiler.depositoGarantia}</p>
+                        <p><strong>Número de contacto:</strong> ${alquiler.nroContacto}</p>
+                    </p>
+                </div>
             </div>
-        </div>
-    </a>
-        
+         </a>
     `;
-
 });
-
 document.getElementById('alquiler').innerHTML = alquileresHTML;
 
 
--- mapa/mapa_iniciar.js
+++ mapa/mapa_iniciar.js
@@ -1,4 +1,5 @@
 var map = L.map('map',).setView([-27.309210095060305,-55.887751579284675], 16 ); 
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
